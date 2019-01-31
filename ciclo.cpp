19b0393758866c9659ad2f06143bce5feb48b1fa
Juan Daniel Fiallos <47200173+jdfiallos@users.noreply.github.com>
2019-01-30 20:40:07 -0600

    Add files via upload

---
+++ ciclo.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	for( int x=0; x=x+1; x=x+1){
+		cout << "Hola" ;
+	}
+	return 0;
+}
+
